164c706a0fdcd03d46ce277c263ed50fc2a2a76f
Chirlanio <56799352+Chirlanio@users.noreply.github.com>
2025-07-19 16:51:39 -0300

    add route getCourses

--- src/controller/courser.js
+++ src/controller/courser.js
@@ -1,15 +1,38 @@
 const { courses } = require("../models");
 
+async function getCourses(req, res) {
+  try {
+    const courses = await courses.findAll({
+      order: ["id", "ASC"],
+    });
+
+    return res.send(courses);
+  } catch (error) {
+    return res.status(500).send({
+      error: error.message,
+    });
+  }
+}
 
 async function CreateCourse(req, res) {
-    try {
-        const { title, description, duration, price, hours, studantes, rating } = req.body;
-        const course = await courses.create({ title, description, duration, price, hours, studantes, rating });
-        return res.status(201).json(course);
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
+  try {
+    const { title, description, duration, price, hours, studantes, rating } =
+      req.body;
+    const course = await courses.create({
+      title,
+      description,
+      duration,
+      price,
+      hours,
+      studantes,
+      rating,
+    });
+    return res.status(201).json(course);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 }
 module.exports = {
-    CreateCourse,
-}
+  CreateCourse,
+  getCourses
+};
--- src/router/course.routes.js
+++ src/router/course.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { CreateCourse } = require("../controller/courser");
+const { CreateCourse, getCourses } = require("../controller/courser");
+
+router.get("/courses", getCourses);
 
 router.post("/courses", CreateCourse);
 
