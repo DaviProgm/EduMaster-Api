e44f668ee256d7307258fd81687f949c60884e9f
Davi Monteiro <d8119514@gmail.com>
2025-07-19 16:37:04 -0300

    create course

---
+++ src/controller/courser.js
@@ -0,0 +1,15 @@
+const { courser } = require("../models");
+
+
+async function CreateCourse(req, res) {
+    try {
+        const { title, description, duration, price, hours, studantes, rating } = req.body;
+        const course = await courser.create({ title, description, duration, price, hours, studantes, rating });
+        return res.status(201).json(course);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+module.exports = {
+    CreateCourse,
+}
